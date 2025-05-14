517eadb25f9749fefc06a9dac9b4f8a3c54eeaa5
Enriquedonaire <donaire.q2@gmail.com>
2025-05-14 14:13:52 +0200

    Add HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import Navbar from './components/Navbar';
@@ -9,25 +8,24 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Reservations from './pages/Reservations';
 import Cabins from './pages/Cabins';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <div className="min-h-screen flex flex-col">
-          <Navbar />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/reservas" element={<Reservations />} />
-              <Route path="/cabanas" element={<Cabins />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/reservas" element={<Reservations />} />
+            <Route path="/cabanas" element={<Cabins />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Provider>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/La-Aldea-Cabins/', // base path para GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
